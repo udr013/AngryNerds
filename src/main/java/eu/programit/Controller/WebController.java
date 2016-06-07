de885b0b91c20bc91b070c7d61a1da96836e9ff1
Rémond Karst <remondkarst@programit.nl>
2016-06-07 16:07:43 +0200

    change webcontroller

--- src/main/java/eu/programit/Controller/WebController.java
+++ src/main/java/eu/programit/Controller/WebController.java
@@ -1,22 +1,17 @@
 package eu.programit.Controller;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by udr013 on 1-6-2016.
  */
-<<<<<<< HEAD
+
 @Controller
-=======
-@RestController
-@ComponentScan
->>>>>>> 21415622fbbbcbe9d39ee62bfd0943fda0619bfc
 public class WebController {
 
-    @RequestMapping("/test")
-    public String Start(){
-        return "index.html";
-    }
+	@RequestMapping("/test")
+	public String Start() {
+		return "index.html";
+	}
 }
