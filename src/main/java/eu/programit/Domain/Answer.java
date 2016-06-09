d0328ae76bab1e4ec81d1021e5d389780ce9a2ca
udr013 <udr013@gmail.com>
2016-06-10 00:03:29 +0200

    new branch created

--- src/main/java/eu/programit/Domain/Answer.java
+++ src/main/java/eu/programit/Domain/Answer.java
@@ -1,8 +1,16 @@
 package eu.programit.domain;
 
-//@Entity
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Answer {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int AnswerID;
 	private String answer;
 	private boolean correct;
 	private String explanation;
--- src/main/java/eu/programit/Domain/Question.java
+++ src/main/java/eu/programit/Domain/Question.java
@@ -15,76 +15,92 @@ public class Question implements Serializable {
 
 	private static final long serialVersionUID = 5801378338363587926L;
 
-    @Id
+	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int questionID;
-    private String question;
-    private String internetLink;
-    //private SourceCode code;
-    //private Answer answers;
-    private boolean isMarked;
-    private Status status;
-    private Difficulty difficulty;
-    private String feedback;
-    private boolean hasFeedback;
+	private String question;
+	private String code;
+	private Difficulty difficulty;
+	private boolean isMarked;
+	private Status status;
+	private String feedback;
+	private boolean hasFeedback;
+	private String internetLink;
+
+
+	public int getQuestionID() {
+		return questionID;
+	}
 
 	public void setQuestionID(int questionID) {
 		this.questionID = questionID;
 	}
 
-	public boolean isHasFeedback() {
-		return hasFeedback;
-	}
-	public void setHasFeedback(boolean hasFeedback) {
-		this.hasFeedback = hasFeedback;
-	}
 	public String getQuestion() {
 		return question;
 	}
+
 	public void setQuestion(String question) {
 		this.question = question;
 	}
-	public String getInternetLink() {
-		return internetLink;
+
+	public String getCode() {
+		return code;
 	}
-	public void setInternetLink(String internetLink) {
-		this.internetLink = internetLink;
+
+	public void setCode(String code) {
+		this.code = code;
 	}
-//	public SourceCode removeCode() {
-//		return code;
-//	}
-//	public void addCode(SourceCode code) {
-//		this.code = code;
-//	}
-//	public Answer showAnswers() {
-//		return answers;
-//	}
-	public boolean getMarked() {
-		return isMarked;
-	}
-	public void setMarked(boolean isMarked) {
-		this.isMarked = isMarked;
-	}
-	public Status getStatus() {
-		return status;
-	}
-	public void setStatus(Status status) {
-		this.status = status;
-	}
+
 	public Difficulty getDifficulty() {
 		return difficulty;
 	}
+
 	public void setDifficulty(Difficulty difficulty) {
 		this.difficulty = difficulty;
 	}
-	public int getQuestionID() {
-		return questionID;
+
+	public boolean isMarked() {
+		return isMarked;
 	}
+
+	public void setMarked(boolean marked) {
+		isMarked = marked;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
 	public String getFeedback() {
 		return feedback;
 	}
+
 	public void setFeedback(String feedback) {
 		this.feedback = feedback;
 	}
-	
+
+	public boolean isHasFeedback() {
+		return hasFeedback;
+	}
+
+	public void setHasFeedback(boolean hasFeedback) {
+		this.hasFeedback = hasFeedback;
+	}
+
+	public String getInternetLink() {
+		return internetLink;
+	}
+
+	public void setInternetLink(String internetLink) {
+		this.internetLink = internetLink;
+	}
 }
+
+
+
+
