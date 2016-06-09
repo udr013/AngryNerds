2d6c5f8edc68122cd2acc19fbec18de8b87c7c47
Carpago Software | Raymond Loman <raymond@carpago.nl>
2016-06-09 15:01:43 +0200

    Add H2 db for unittest environment and add application-unittest.properties

--- src/test/java/eu/programit/AngryNerdsApplicationTests.java
+++ src/test/java/eu/programit/AngryNerdsApplicationTests.java
@@ -3,12 +3,14 @@ package eu.programit;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AngryNerdsApplication.class)
 @WebAppConfiguration
+@ActiveProfiles("unittest")
 public class AngryNerdsApplicationTests {
 
 	@Test
