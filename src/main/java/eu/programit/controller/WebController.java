05e2995a159005c66b80612432434a1ea06a7882
udr013 <udr013@gmail.com>
2016-06-10 15:06:30 +0200

    deleted SourceCode.java as it's replaced with a string in Question

--- src/main/java/eu/programit/controller/WebController.java
+++ src/main/java/eu/programit/controller/WebController.java
@@ -71,6 +71,56 @@ public class WebController {
         return "question";
     }
 
+    @RequestMapping("/registerQuestion")
+    String saveQuestion(Model model){
+        Question q = new Question();
+        q.setQuestionID(12);
+        q.setQuestion("Would the following code compile?");
+        q.setDifficulty(Difficulty.EASY);
+        q.setCode("package eu.programit.domain;\n" +
+                "\n" +
+                "//@Entity\n" +
+                "public class Category {\n" +
+                "\t\n" +
+                "\tprivate String name;\n" +
+                "\tprivate String chapter;\n" +
+                "\tprivate Question questions;\n" +
+                "\t\n" +
+                "\tpublic String getChapter() {\n" +
+                "\t\treturn chapter;\n" +
+                "\t}\n" +
+                "\tpublic void setChapter(String chapter) {\n" +
+                "\t\tthis.chapter = chapter;\n" +
+                "\t}\n" +
+                "\tpublic Question getQuestions() {\n" +
+                "\t\treturn questions;\n" +
+                "\t}\n" +
+                "\tpublic void setQuestions(Question questions) {\n" +
+                "\t\tthis.questions = questions;\n" +
+                "\t}\n" +
+                "\tpublic String getName() {\n" +
+                "\t\treturn name;\n" +
+                "\t}\n" +
+                "\tpublic void setName(String name) {\n" +
+                "\t\tthis.name = name;\n" +
+                "\t}\n" +
+                "\t\n" +
+                "\t\n" +
+                "\n" +
+                "}\n");
+        model.addAttribute("question", q);
+        Answer a = new Answer();
+        a.setAnswer("this is an invalid class");
+        Answer b = new Answer();
+        b.setAnswer("this class will not be recognized in springboot mysql");
+        List<Answer> answers = new ArrayList<>();
+        answers.add(a);
+        answers.add(b);
+        model.addAttribute("answers", answers);
+
+        return "question";
+    }
+
 
 
 
