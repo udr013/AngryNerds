379302201ed5bb1c8eab6f786f5390c0c0b00bb4
Rik van de Maat <rvandemaat@outlook.com>
2016-06-13 15:41:01 +0200

    Changes made to QuestionRepository.java

--- src/main/java/eu/programit/repository/QuestionRepository.java
+++ src/main/java/eu/programit/repository/QuestionRepository.java
@@ -1,12 +1,25 @@
 package eu.programit.repository;
 
-import eu.programit.domain.Question;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 
-/**
- * Created by udr013 on 8-6-2016.
- */
+import eu.programit.domain.Question;
+
 @Component
-public interface QuestionRepository extends CrudRepository<Question,Integer> {
+public interface QuestionRepository extends CrudRepository<Question, Long> { 
+	
 }
+
+
+//package eu.programit.repository;
+//
+//import eu.programit.domain.Question;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Component;
+//
+///**
+//* Created by udr013 on 8-6-2016.
+//*/
+//@Component
+//public interface QuestionRepository extends CrudRepository<Question,Integer> {
+//}
