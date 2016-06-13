82582fb357503d17fceed5cb90345bf6a170ff05
Rémond Karst <remondkarst@programit.nl>
2016-06-13 15:39:31 +0200

    QuestionService aangemaakt

--- src/main/java/eu/programit/service/QuestionService.java
+++ src/main/java/eu/programit/service/QuestionService.java
@@ -1,14 +1,47 @@
 package eu.programit.service;
 
-import eu.programit.repository.QuestionRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import eu.programit.domain.Question;
+import eu.programit.repository.QuestionRepository;
 
 /**
  * Created by udr013 on 9-6-2016.
  */
+@Service
+@Transactional
 public class QuestionService {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(QuestionService.class);
+	
+	@Autowired
+	private QuestionRepository questionRepository;
 
-    @Autowired
-    QuestionRepository questionRepository;
-    
+	
+	public Question save(Question question) {
+		
+		LOGGER.debug("it rocks");
+		
+		// aanroep van het door Mockito gemaakte contract (normaliter zet je dat natuurlijk hier
+//		niet in de code 
+		Question result = this.questionRepository.save(question);	
+		
+		return result;
+
+	}
+	
+	public Iterable<Question> findAll() {
+		Iterable<Question> result = this.questionRepository.findAll();
+		
+		return result;
+	}
+
+	
+
+
 }
