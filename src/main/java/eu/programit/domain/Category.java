98bcaeb399d10d954a3d44cb277a9f6654a34bda
Rémond Karst <remondkarst@programit.nl>
2016-06-13 14:11:35 +0200

    entity aangezet bij Category

--- src/main/java/eu/programit/domain/Category.java
+++ src/main/java/eu/programit/domain/Category.java
@@ -1,31 +1,42 @@
 package eu.programit.domain;
 
-//@Entity
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Category {
-	
+	// technical key
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 	private String name;
 	private String chapter;
 	private Question questions;
-	
+
 	public String getChapter() {
 		return chapter;
 	}
+
 	public void setChapter(String chapter) {
 		this.chapter = chapter;
 	}
+
 	public Question getQuestions() {
 		return questions;
 	}
+
 	public void setQuestions(Question questions) {
 		this.questions = questions;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
 
 }
