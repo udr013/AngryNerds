7fe887f6ee13a753c9abf4f76b7f5bfd00fe5cf2
udr013 <udr013@gmail.com>
2016-06-16 13:06:40 +0200

    hier de controller, wil iemand van jullie die hernoemen naar NewQuestionController, als ik dat hier doe geeft dat problemen

--- src/main/java/eu/programit/controller/newQuestionController.java
+++ src/main/java/eu/programit/controller/newQuestionController.java
@@ -31,23 +31,23 @@ public class newQuestionController {
     public String saveQuestion(@ModelAttribute("question") Question question, Model model) {
         System.out.println(question);
         iQuestionService.saveQuestion(question);
-//        List<Question> questions = (List<Question> )iQuestionService.findAll();
-//        Question addedQuestion = questions.get(questions.size()-1);
-//        System.out.println(addedQuestion);
-//        model.addAttribute("question",addedQuestion);
-//        Answer nextAnswer = new Answer(addedQuestion);
         model.addAttribute("answer", new Answer());
         return "answers";
     }
 
     @RequestMapping(value = "/answersave", method = RequestMethod.POST)
-    public String saveAnswer(@ModelAttribute("answer") Answer answer) {
+    public String saveAnswer(@ModelAttribute("answer") Answer answer, Model model) {
         List<Question> questions = (List<Question> )iQuestionService.findAll();
         Question addedQuestion = questions.get(questions.size()-1);
         answer.setQuestion(addedQuestion);
         System.out.println(answer);
         iAnswerService.saveAnswer(answer);
+        List<Answer> answers = addedQuestion.getAnswers();
+        model.addAttribute("question", addedQuestion);//the 1 will get question 2 (index 0)
+        model.addAttribute("answer", new Answer());
+        model.addAttribute("answers", answers);
 
         return "answers" ;
     }
+
 }
