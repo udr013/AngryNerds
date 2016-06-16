f02bac65c8269e83ce82200c273a28ec06e14c2d
Rémond Karst <remondkarst@programit.nl>
2016-06-16 14:51:50 +0200

    nu werkt die echt werkelijk waar!!!!!

--- src/main/java/eu/programit/controller/newCategoryController.java
+++ src/main/java/eu/programit/controller/newCategoryController.java
@@ -1,7 +1,5 @@
 package eu.programit.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,17 +7,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Created by udr013 on 16-6-2016.
- */
-
-import eu.programit.domain.Answer;
 import eu.programit.domain.Category;
-import eu.programit.domain.Question;
 import eu.programit.service.ICategoryService;
 
 @Controller
-public class newCategoryController {
+public class NewCategoryController {
 
 
     @Autowired
