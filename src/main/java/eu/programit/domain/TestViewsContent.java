5b3d5bdd926cc923c625b8695d7718933dde2513
Leandro <github@mittere.net>
2016-06-23 15:36:15 +0200

    Fixed nullpointer exception and some html sutff

--- src/main/java/eu/programit/domain/TestViewsContent.java
+++ src/main/java/eu/programit/domain/TestViewsContent.java
@@ -3,11 +3,13 @@ package eu.programit.domain;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 /*
 * This table is used by table TestViews.
@@ -31,6 +33,13 @@ public class TestViewsContent implements Serializable {
 	Integer orderNr;	// defines nr of the question within the test
 	Integer questionId;	// Id of question
 	
+	/****** Vars  NOT in DB   ******************************************************/
+/*	
+	@Transient
+	@OneToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="myQuestionId")
+	private Question question;
+*/	
 	/****** Getters & Setters ******************************************************/
 	
 	public TestViews getTestViews() {
