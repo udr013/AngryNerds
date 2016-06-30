eb9cf8c3ccf1cd7e7207696956433132a03fa769
Carpago Software | Raymond Loman <raymond@carpago.nl>
2016-06-30 10:27:51 +0200

    Remove unused class or for me it seems unused. App and security ok after deleting this

--- src/main/java/eu/programit/config/SecurityInitializer.java
+++
@@ -1,9 +0,0 @@
-package eu.programit.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-/**
- * Created by udr013 on 12-5-2016.
- */
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
