f4cf36201d3b785c1fa299b7971ef2827a8cac31
RemondKarst <remond.karst@gmail.com>
2016-07-13 11:14:41 +0200

    Weer een stukje verder het probleem zit volgens mij in de HTML pagina

--- src/main/java/eu/programit/controller/ExamController.java
+++ src/main/java/eu/programit/controller/ExamController.java
@@ -30,22 +30,33 @@ public class ExamController {
     IQuestionService iQuestionService;
 
     @RequestMapping(value = "/examsave", method = RequestMethod.POST)
-    String createExam(@ModelAttribute("testviews") TestViews testViews, Model model) {
+    String createExam(@ModelAttribute("testviews") TestViews testViews, Model model,ExamQuestionSelectedForm examQuestionSelectedForm) {
         System.out.println("this is createExam" + testViews);
+        Question q;
         iTestViewsService.saveTestViews(testViews);
         List<TestViews> testViewses= (List<TestViews>) iTestViewsService.findAll();
         TestViews latestTest = testViewses.get(testViewses.size()-1);
         testViews = iTestViewsService.findById(latestTest.getId());
+      //  ExamQuestionSelectedForm examQuestionSelectedForm = new ExamQuestionSelectedForm();
+        try {
+			q = iQuestionService.findById(myTestView.getCurrentQuestion().getQuestionId());
+		//	answers = q.getAnswers();
+		} catch (NullPointerException npe) {
+			q = new Question();
+			q.setContent("Unknown question requested (questionID does not exist)");
+		}
         model.addAttribute("latesttestview",testViews );
         model.addAttribute("questions", iQuestionService.findAll());
+        model.addAttribute("examQuestionSelectedForm",examQuestionSelectedForm);
         model.addAttribute("selectedQuestions", new ArrayList<Question>() {
         });
-        ExamQuestionSelectedForm examQuestionSelectedForm = new ExamQuestionSelectedForm();
+        examQuestionSelectedForm.setTestAnswers(mySelectedQuestions.getTestViewsContent().get(q.getQuestionID()));
+		model.addAttribute("mySelectedQuestions", mySelectedQuestions.getTestViewsContent().get(q.getQuestionID()));
         return toTheAddQuestionToExamPage;
     }
 
     @RequestMapping(value = "/saveQuestToExam", method = RequestMethod.POST)   
-    public String saveQuestToExam(@ModelAttribute ExamQuestionSelectedForm examQuestionSelectedForm ) {
+    public String saveQuestToExam(@ModelAttribute("examQuestionSelectedForm") ExamQuestionSelectedForm examQuestionSelectedForm ) {
     	System.out.println("in save question to exam"+examQuestionSelectedForm);
     	mySelectedQuestions.setTestViewsContent(new Integer(myTestView.getId()),
 				examQuestionSelectedForm.getSelectedQuestions());
